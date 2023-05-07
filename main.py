0fac64bf82b6ad3743a52415b2267aa64f1dc280
Jason Lee <8713509+jay-lee-code@users.noreply.github.com>
2023-05-07 16:14:23 -0700

    Wrote starter skeleton code for service

---
+++ main.py
@@ -0,0 +1,51 @@
+import requests
+import time
+import argparse
+import sqlite3
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+con = sqlite3.connect("cache.db")
+cur = con.cursor()
+
+# described in seconds:
+max_elapsed = 60 * 60 * 24 * 30
+
+
+def create_table():
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS web_requests (url TEXT PRIMARY KEY, data BLOB, timestamp INTEGER);"
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Start simple request cache service.'
+    )
+    parser.add_argument(
+        "--port", help="port to use",
+        type=int,
+        required=False
+    )
+    parser.add_argument(
+        "--baseurl",
+        help="base url prefix for all requests",
+        required=False
+    )
+    parser.add_argument(
+        "--nodebug",
+        action="store_false",
+        help="disables hot reloading and debug messages",
+        required=False
+    )
+
+    args = parser.parse_args()
+
+    if args.port:
+        port = args.port
+    else:
+        port = 5123
+
+    create_table()
+    app.run(port=port, debug=args.nodebug)
